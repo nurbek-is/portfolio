972898eb60b5323f9fd48b0a7e2831267a1d7029
Nurbek Ismailov <nurbek82@gmail.com>
2017-04-04 18:31:21 -0700

    starting building js fn

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+var allArticles = [];
+
+function Article (opts) {
+  this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.title = opts.title;
+  this.category = opts.category;
+  this.body = opts.body;
+  this.publishedOn = opts.publishedOn;
+}
