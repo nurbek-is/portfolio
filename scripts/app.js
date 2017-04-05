69b7e3106a1597746dc57ba0bec98e73a74301f6
Nurbek Ismailov <nurbek82@gmail.com>
2017-04-05 10:05:24 -0700

    added time template

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,12 @@
-var allArticles = [];
+alert('from app.js')
+var allProjects = [];
 
-function Article (opts) {
-  this.author = opts.author;
-  this.authorUrl = opts.authorUrl;
+function Projects (opts) {
   this.title = opts.title;
   this.category = opts.category;
+  this.gitHubUrl = opts.gitHubUrl;
   this.body = opts.body;
   this.publishedOn = opts.publishedOn;
 }
+Article.prototype.toHtml = function() {
+  var $newArticle = $('article.template').clone().removeClass('template')
