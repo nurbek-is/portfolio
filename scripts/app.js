9c7cef743d09d7bf29fc29ebeecbbeb9a6af53cc
Nurbek Ismailov <nurbek82@gmail.com>
2017-04-09 12:16:23 -0700

    clone is working,for loop is printing items in array

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,35 @@
-alert('from app.js')
 var allProjects = [];
+var allProjectString = [];
 
-function Projects (opts) {
+function Project (opts) {
   this.title = opts.title;
+  this.author = opts.author;
   this.category = opts.category;
   this.gitHubUrl = opts.gitHubUrl;
   this.body = opts.body;
   this.publishedOn = opts.publishedOn;
 }
-Article.prototype.toHtml = function() {
-  var $newArticle = $('article.template').clone().removeClass('template')
+$(document).ready(function(){
+  $(".projectClass").clone().appendTo(".projectClassSection");
+  $(".projectClass").clone().appendTo(".projectClassSection");
+});
+
+Project.prototype.toHtml = function () {
+  // var $newProject = $("#projectSection").clone().apendTo("body");
+
+  // $('article.projectClass').clone();
+  // $newProject.data('category', this.category);
+  // $newProject.find('h1').text(this.title);
+  // return $newProject;
+};
+sourceProjects.forEach(function(project) {
+  new Project(project).toHtml();
+  allProjects.push(project);
+});
+
+for (var i = 0; i < allProjects.length; i++){
+  var projectStr = allProjects[i].title + ", " + "<br>" +  allProjects[i].author + ", " + "<br>" +
+  allProjects[i].category + ", " + "<br>" + allProjects[i].gitHubUrl;
+  console.log (projectStr)
+  $('#divResults').html(projectStr);
+};
