c1c9ef0958172e3ad0600ca31c0ee6eb260de91f
Nurbek Ismailov <nurbek82@gmail.com>
2017-04-09 22:23:50 -0700

    added some style to new tiles

--- scripts/app.js
+++ scripts/app.js
@@ -10,32 +10,18 @@ function Project (opts) {
 }
 $(document).ready(function(){
   // var $newProject = $(".projectClass").clone().appendTo(".projectClassSection");
-  // $(".projectClass").clone().appendTo(".projectClassSection");
-  // $(".projectClass").clone().appendTo(".projectClassSection");
-
   var projectStr = "";
   for (var i = 0; i < allProjects.length; i++){
-    projectStr += "<article id='project1' class='projectClass'>" + allProjects[i].title + ", " + "<br>" +  allProjects[i].language + ", " + "<br>" +
-    allProjects[i].category + ", " + "<br>" + allProjects[i].gitHubUrl + "<br>" + "<" + '/' + 'article' + ">";
+    projectStr += "<article id='project1' class='projectClass'>" + allProjects[i].title + "<br>" +  allProjects[i].language + ", " + "<br>" +
+    allProjects[i].category + '<br>' + allProjects[i].publishedOn + "<br>" + ", " + "<br>" + allProjects[i].gitHubUrl + "<br>" + "<" + '/' + 'article' + ">" + "<br>";
     $(".projectClassSection").append(projectStr);
     projectStr = "";
-    }
-
-
-  //  $(".projectClass").clone().appendTo(".projectClassSection");
-  // $(".projectClass").appendTo(".projectClassSection");
-  //   var projectStr = allProjects[i].title + ", " + "<br>" +  allProjects[i].language + ", " + "<br>" +
-  //   allProjects[i].category + ", " + "<br>" + allProjects[i].gitHubUrl + "<br>";
-  //   console.log (projectStr)
-  //   //  $('.projectClassSection').append(projectStr);
-  //    $("projectStr").html(".projectClassSection");
-  // };
+  }
 });
 
 Project.prototype.toHtml = function () {
   //   var $newProject = $(".projectClass").clone();
-  //   $('section.projectClassSection').removeClass("projectClassSection");
-   //
+  // $('section.projectClassSection').removeClass("projectClassSection");
   //  $newProject.find('h1').text(this.title);
   //  $newProject.find('section.article-body').text(this.body);
   //  return $newProject;
@@ -43,6 +29,6 @@ Project.prototype.toHtml = function () {
 sourceProjects.forEach(function(project) {
   allProjects.push(new Project(project));
 });
-allProjects.forEach(function(a) {
-  $(".projectClassSection").append(a.toHtml());
-});
+// allProjects.forEach(function(a) {
+  // $(".projectClassSection").append(a.toHtml());
+// });
