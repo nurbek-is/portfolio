37c661fb70beef72e03ad92e91abe48607934e47
Nurbek Ismailov <nurbek82@gmail.com>
2017-04-16 20:31:44 -0700

    added template handlebars

--- scripts/app.js
+++ scripts/app.js
@@ -9,18 +9,22 @@ function Project (opts) {
   this.publishedOn = opts.publishedOn;
 }
 Project.prototype.toHtml = function () {
-  var $newProject = $('article.article-class').clone();
-  $newProject.removeClass('article-class');
-  $newProject.find('h1').html(this.title);
-  $newProject.find('h2').html(this.language);
-  $newProject.attr('data-language', this.language)
-  $newProject.find('h3').html(this.category);
-  $newProject.attr('data-category', this.category);
-  $newProject.find('a.atag').attr('href',this.gitHubUrl);
-  $newProject.find('a.atag').text(this.gitHubUrl);
-  $newProject.find('section.body-class').html(this.body);
-  $newProject.find('time.time-class').html(this.publishedOn);
-   return $newProject;
+  // var $newProject = $('article.article-class').clone();
+  // $newProject.removeClass('article-class');
+  // $newProject.find('h1').html(this.title);
+  // $newProject.find('h2').html(this.language);
+  // $newProject.attr('data-language', this.language)
+  // $newProject.find('h3').html(this.category);
+  // $newProject.attr('data-category', this.category);
+  // $newProject.find('a.atag').attr('href',this.gitHubUrl);
+  // $newProject.find('a.atag').text(this.gitHubUrl);
+  // $newProject.find('section.body-class').html(this.body);
+  // $newProject.find('time.time-class').html(this.publishedOn);
+  //  return $newProject;
+  var $source = $('#portfolioHandlebar').html();
+  var template = Handlebars.compile($source);
+  debugger;
+  return template(this);
 };
 sourceProjects.forEach(function(project) {
   allProjects.push(new Project(project));
